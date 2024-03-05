6e8a9d16b5500906e159c72674f750025e5f0524
David Shore <david@foggymedia.com>
2024-03-05 10:15:09 +0100

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./question6/App";
+import App from "./question1/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
