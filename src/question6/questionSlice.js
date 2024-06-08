e6d1989b14819f12c4aeadb1fd22bd7e3470cd70
Peywan Sabir <peywan1999@icloud.com>
2024-06-08 23:41:45 +0200

    ss

--- src/question6/questionSlice.js
+++ src/question6/questionSlice.js
@@ -1,19 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const questionSlice = createSlice({
+const initialState = {
+  question: "",
+  answer: ""
+};
+
+const questionSlice = createSlice({
   name: "question",
-  initialState: {
-    question: "",
-    answer: "",
-  },
+  initialState,
   reducers: {
     setQuestion: (state, action) => {
       state.question = action.payload;
     },
     setAnswer: (state, action) => {
       state.answer = action.payload;
-    },
-  },
+    }
+  }
 });
 
 export const { setQuestion, setAnswer } = questionSlice.actions;
